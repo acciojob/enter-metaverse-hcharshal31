0918fe5d36d7193afaecea402cf348da91f041ea
Harshal Chavan <91681985+hcharshal31@users.noreply.github.com>
2025-01-03 15:46:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+const metaVerse = document.getElementById("status");
+const enterButton = document.getElementById("enterBtn");
+const entered = document.querySelector("h1");
+
+enterButton.onclick = function(){
+	metaVerse.style.display = "none";
+	entered.style.display = "block";
+}
